adb6ce61f09b9deb48f3764842a4604b3da018ee
MartinChavire <MartinChavire@users.noreply.github.com>
2023-10-23 18:14:08 +0000

    deploy: c7945333325c3bced6a4c158d2c0d97622810110

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['moveradius_0',['moveRadius',['../db/d41/namespaceMOVE.html#a977e6d354da3e79a406d2e9610858eea',1,'MOVE']]]
+  ['move_0',['move',['../db/d41/namespaceMOVE.html#a4c2535d28abd5a94e9dbaed21a4a62b3',1,'MOVE']]],
+  ['moveradius_1',['moveRadius',['../db/d41/namespaceMOVE.html#a977e6d354da3e79a406d2e9610858eea',1,'MOVE']]]
 ];
