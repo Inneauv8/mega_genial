ee8e27883a4b77c379eeb601d65275ff695a9972
Mathieu Durand <mathieu_durand@icloud.com>
2023-10-25 14:42:40 -0400

    Added missing function to Move.h, way too many missing (impossible to use outside of move.cpp)

--- src/Move.h
+++ src/Move.h
@@ -1,8 +1,8 @@
 /****************************************************************************************
 Nom du fichier : MOVE.h
-Auteur :  Guillaume Béland et Samuel Hamelin                 
+Auteur :  Guillaume Béland et Samuel Hamelin
 Date de création : 17/10/2023
-  
+
 ****************************************************************************************/
 #ifndef MOVE_H
 #define MOVE_H
@@ -17,60 +17,65 @@ namespace MOVE {
   #define WHEEL_BASE_DIAMETER 7.480315
   #define WHEEL_DIAMETER 2.992126
 
-
   extern float pulseToDist;
 
   // *************************************************************************************************
   //  STRUCTURES ET UNIONS
   // *************************************************************************************************
-  struct valeursPID 
-  {
-      valeursPID() : Kp(0.0), Ki(0.0), Kd(0.0), Ti(0.0), Sp(0.0), Pv(0.0) {}
-      float Kp; // Constante proportionnelle
-      float Ki; // Constante intégrale
-      float Kd; // Constante dérivée
-      float Ti; // Temps initial
-      float dt; // Intervalle de temps pour le calcul d'intégrales et de dérivées
-      float Sp; // Set Point (Valeur voulue)
-      float Pv; // Process Value (Valeur réelle)
-      float p; // Valeur proportionnelle
-      float i; // Valeur intégrale
-      float d; // Valeur dérivée
-      float Out; // Valeur de sortie
+  struct valeursPID {
+    valeursPID() : Kp(0.0), Ki(0.0), Kd(0.0), Ti(0.0), dt(0.0), Sp(0.0), Pv(0.0) {}
+    float Kp;  // Constante proportionnelle
+    float Ki;  // Constante intégrale
+    float Kd;  // Constante dérivée
+    float Ti;  // Temps initial
+    float dt;  // Intervalle de temps pour le calcul d'intégrales et de dérivées
+    float Sp;  // Set Point (Valeur voulue)
+    float Pv;  // Process Value (Valeur réelle)
+    float p;   // Valeur proportionnelle
+    float i;   // Valeur intégrale
+    float d;   // Valeur dérivée
+    float Out; // Valeur de sortie
   };
 
   struct valeursDistance {
     float G;
     float D;
   };
-  
-  struct posRobot {
-      float x;
-      float y;
-      float orientation;
 
+  struct posRobot {
+    float x;
+    float y;
+    float orientation;
   };
 
   extern valeursDistance Distance;
   extern posRobot position;
-  
+
   // *************************************************************************************************
   //  PROTOTYPE DE FONCTIONS
   // *************************************************************************************************
-  
+
   void calculPID(valeursPID *incomingValues);
-  
-  valeursDistance getDistance();
+  void updatePos();
+  float speedG();
+  float speedD();
+  float averageSpeedG();
+  float averageSpeedD();
+  float speedToVoltage(bool motor, float speed);
+  float moveRadius(float xFinal, float yFinal, float finalOrientation);
+  void showDataPID(valeursPID *incomingValues);
+  void updatePIDG(float Sp);
+  void updatePIDD(float Sp);
+  void updatePIDMain(float speed, float dV);
+  float radiusToSpeedG(float moveRadiusRobot, float finalOrientation);
+  float radiusToSpeedD(float moveRadiusRobot, float finalOrientation);
+  void move(float xFinal, float yFinal, float finalOrientation);
 
-  float speed(bool moteur);
-
-  float distanceMoyenne();
-  
   // *************************************************************************************************
   // VARIABLES GLOBALES
   // *************************************************************************************************
   /* VIDE */
-  
+
   // *************************************************************************************************
   // VARIABLES LOCALES
   // *************************************************************************************************
